f3049b1bb2ea617341903024f73b9e9cbf75833e
= <ishu.kool2406@gmail.com>
2025-04-12 18:08:04 +0530

    setup

---
+++ src/test/java/com/example/DairyEntry/DairyEntryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DairyEntry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DairyEntryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
